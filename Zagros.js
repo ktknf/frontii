95dc567666a5b6a00a6b514ded0e50359179ef19
root <root@ip-95-217-5-6.privatedns.ws>
2019-08-18 19:36:39 +0000

    gate added

--- Zagros.js
+++ Zagros.js
@@ -11,14 +11,27 @@ module.exports = {
       Source + "&cbTarget=" + Target +
       "&cbDay1=28&cbMonth1=5&cbAdultQty=1&cbChildQt=0&InfantQty=0&OfficeUser=THR210-1.WS&OfficePass=K2019";
     request.get(get_url, function(err, res, body) {
-          var flght = JSON.parse(body);
-          console.log(flght);
+          var flght = JSON.parse(body)['AvailableFlights'];
+          for(var i=0; i<flght.lenght;i++)
+          {
+            console.log(flght[0].ClassesStatus);
+          }
           callback(body);
     });
   },
   //GetFlight Function End
 
-  ToPersianDigits: function() {
-    return "Hola";
+//Reserve Function Start
+  Reserve: function(Source,Target,FlightClass,No,Day,Month,Name,Last,Age,ID,FlightNo,Contact,callback) {
+    var get_url = "http://book.zagrosairlines.com/cgi-bin/NRSWeb.cgi/ReservJS?Airline=ZV"
+    +"&cbSource="+Source+"&cbTarget="+Target+"&FlightClass="+FlightClass+"&No="+No+"&Day="
+    +Day+"&Month="+Month+"&edtName1="+Name+"&edtLast1="+Last+"&edtAge1="+Age+"&edtID1="
+    +ID+"&OfficeUser=THR210-1.WS&OfficePass=K2019&edtContact="+Contact+"&FlightNo="+FlightNo;
+
+    request.get(get_url, function(err, res, body) {
+
+          callback(body);
+    });
   }
+  //Reserve Fuvnction End
 };
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const request = require("request");
+const bodyParser = require("body-parser");
 const sendmail = require('sendmail')();
 var fs = require('fs');
 var path = require('path');
@@ -22,13 +23,16 @@ const port = 29
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
 
-	Zagros.GetFlights('MHD','THR','','', function(result)
+	Zagros.Reserve('MHD','THR','ID','1','28','5','Mohammad','Ahmadi','22','117000998833','4030','123123123', function(result)
 	{
+		console.log(result);
 	});
 
 })
@@ -36,5 +40,14 @@ app.get('/', function (req, res) {
 app.get('/soon', function (req, res) {
   res.render('main.ejs');
 })
+
+//Payment Result
+app.post('/payres', function (req, res) {
+	if(req.body.State==='OK')
+	{
+		console.log(req.body);
+	}
+})
+//End of Payment Result
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
