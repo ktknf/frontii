158498d9a6830be52d92b0e6e815387a80db879d
root <root@ip-95-217-5-6.privatedns.ws>
2019-09-02 14:14:11 +0000

    6.11.18

--- Iati.js
+++ Iati.js
@@ -1,10 +1,25 @@
 var request = require('request');
 var zlib = require("zlib");
+var moment = require('moment-jalaali');
 let {
   PythonShell
 } = require('python-shell')
 //Modules
 var utility = require("./utility.js");
+
+var month_before = Array('/۰۱/', '/۰۲/', '/۰۳/', '/۰۴/', '/۰۵/', '/۰۶/', '/۰۷/', '/۰۸/', '/۰۹/','/۱۰/','/۱۱/','/۱۲/');
+var month = Array(' فروردین ',
+  ' اردیبهشت ',
+  ' خرداد ',
+  ' تیر ',
+  ' مرداد ',
+  ' شهریور ',
+  ' مهر ',
+  ' آبان ',
+  ' آذر ',
+  ' دی ',
+  ' بهمن ',
+  ' اسفند ');
 
 module.exports = {
 
@@ -17,19 +32,49 @@ module.exports = {
       var flght = JSON.parse(body);
       var final_return = [];
       for (var i = 0; i < flght.length; i++) {
+
+        var dtime=flght[i]['Legs'][0]['DepartureTime'];
+        var final_dtime=dtime.split(' ')[0];
+
+        m = moment(final_dtime, 'YYYY-M-D');
+        m.locale('fa').format('YYYY/MM/DD');
+        var dret=m.locale('fa').format('jDD/jMM/jYYYY')+"   "+dtime.split(' ')[1]+":00";
+
+        for(var j=0;j<12;j++)
+        {
+          dret=dret.replace(month_before[j],month[j]);
+        }
+
+
+        var atime=flght[i]['Legs'][0]['ArrivalTime'];
+        var final_atime=atime.split(' ')[0];
+
+        m = moment(final_atime, 'YYYY-M-D');
+        m.locale('fa').format('YYYY/MM/DD');
+        var aret=m.locale('fa').format('jDD/jMM/jYYYY')+"   "+atime.split(' ')[1]+":00";
+
+        for(var j=0;j<12;j++)
+        {
+          aret=aret.replace(month_before[j],month[j]);
+        }
+
+
+//console.log(flght[i]['Legs'][0]['Baseprice']);
+          var prstr=Math.floor(flght[i]['Baseprice']/10000)*1000;
+
         //console.log(flght[i]);
           final_return.push({
             AirLine:flght[i]['Legs'][0]['OperatorName'],
             AirLineShort:flght[i]['Legs'][0]['OperatorCode'],
             TimeClass:'noon',
-            DepartureDateTime:flght[i]['Legs'][0]['DepartureTime'],
-            ArrivalDateTime:flght[i]['Legs'][0]['ArrivalTime'],
+            DepartureDateTime:dret,
+            ArrivalDateTime:aret,
             From:Source,
             FullFrom:Source,
             To:Target,
             FullTo:Target,
-            Price:utility.CommaSeprate(10000000),
-            IntPrice:parseInt(1000000),
+            Price:utility.CommaSeprate(prstr),
+            IntPrice:parseInt(flght[i]['Baseprice']),
             FlightNo:flght[i]['Legs'][0]['FlightNo'],
             Class:'x',
             Spec:Source+"-"+Target+"-"+flght[i]['Legs']['FlightNo']+"-"+"X"+"-"+Day+"-"+Month+"-"+"100000"+"-IV"
